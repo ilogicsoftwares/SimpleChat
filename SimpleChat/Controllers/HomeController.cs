a5045a7dfe7c43cf88c57043b7526957dae898af
Richard Aguirre <richardjap@gmail.com>
2016-08-19 03:45:59 -0430

    Init

---
+++ SimpleChat/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleChat.Models;
+
+namespace SimpleChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Chat(Chat chat)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.canal = chat.canal;
+                ViewBag.user = chat.user;
+                return View();
+            }else
+            {
+                return View("Index");
+            }
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SimpleChat/Models/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleChat.Models
+{
+    public class Chat
+    {
+        [Required(ErrorMessage ="Debe incluir un nombre de usuario")]
+        public string canal { get; set; }
+        [Required(ErrorMessage = "Debe incluir un nombre de Canal")]
+        public string user { get; set; }
+    }
+}
